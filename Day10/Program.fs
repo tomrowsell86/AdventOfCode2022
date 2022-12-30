e14c2fb2d253b6d85f76c2e614e6922776c305ff
Tom Rowsell <rowselltom@gmail.com>
2022-12-30 23:10:20 +0000

    Now renders correctly

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -13,9 +13,7 @@ let (|NoOp|Add|) (cmd: string) =
     | _ -> NoOp
 
 let mapToRecordingCycle (cycle, register) =
-    //reg value updated after they cycles have ran so need to -1 to get the in progress reg value for a given cycle
-    let ranges = [ 19..40..219 ]
-
+    let ranges = [ 20..40..220 ]
 
     let recordedCycleResult =
         List.tryPick (fun x -> if x = cycle then Some(x) else None) ranges
@@ -23,54 +21,42 @@ let mapToRecordingCycle (cycle, register) =
     if recordedCycleResult = None then
         0
     else
-        (recordedCycleResult.Value + 1) * register
+        (recordedCycleResult.Value) * register
 
-let folder (state: List<int * int> * int) (line: string) =
-    let (registerCycleMap, currentCycle) = state
-    let lastRecording = List.tryHead registerCycleMap
-
-    let registerVal =
-        if lastRecording = None then
-            1
-        else
-            let (_, reg) = lastRecording.Value
-            reg
+let folder (state: List<int * int> * int * int) (line: string) =
+    let (registerCycleMap, currentCycle, registerVal) = state
 
     match (line) with
-    | NoOp -> (((currentCycle, registerVal) :: registerCycleMap), currentCycle + 1)
+    | NoOp -> (((currentCycle, registerVal) :: registerCycleMap), currentCycle + 1, registerVal)
     | Add op ->
-        ([ (currentCycle + 1, registerVal + op); ((currentCycle, registerVal)) ]
+        ([ (currentCycle + 1, registerVal); ((currentCycle, registerVal)) ]
          @ registerCycleMap,
-         currentCycle + 2)
+         currentCycle + 2,
+         registerVal + op)
 
-let (result, _) = Seq.fold folder ([], 1) input
+let (result, _, _) = Seq.fold folder ([], 1, 1) input
 
-let isOnSprite rowLength registerVal  = 
-    match rowLength+1 - registerVal with
-    | 1 | 0 | -1 -> true
-
+let isOnSprite rowLength registerVal =
+    match rowLength - registerVal with
+    | 1
+    | 0
+    | -1 -> true
     | _ -> false
 
-let renderFolder (state:list<string>*list<char>) cycleRegister = 
+let renderFolder (state: list<string> * list<char>) cycleRegister =
     let (_, regValue) = cycleRegister
-    let (rows,currentRow) = state
+    let (rows, currentRow) = state
     let rowLength = List.length currentRow
-    let pixel = 
-        if (isOnSprite (rowLength) regValue) then
-            '#'
-        else
-            '.'
+    let pixel = if (isOnSprite (rowLength) regValue) then '#' else '.'
+
     if rowLength = 39 then
-        let newRow = new string(Array.ofList (currentRow@[pixel]))
-        (rows@[newRow], [])
+        let newRow = new string (Array.ofList (currentRow @ [ pixel ]))
+        (rows @ [ newRow ], [])
     else
-        (rows, currentRow@[pixel])
-
-
-
+        (rows, currentRow @ [ pixel ])
 
 List.iter (fun (c, r) -> printfn " cycle : %d reg: %d" c r) result
-let (rows,_) = List.fold renderFolder ([],[]) ((List.rev result)) 
+let (rows, _) = List.fold renderFolder ([], []) ((List.rev result))
 List.iter (fun x -> printfn "%s" x) rows
 let signalSum = List.map mapToRecordingCycle result |> List.sum
 printfn "Signal is %d" signalSum
